8c586e1727148e81a37dd2e5b3a14eefd0b2d780
Sunny <curiosity654@outlook.com>
2020-05-19 16:41:23 +0800

    add toast

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
 "assets/assets/logo.png": "2c71de2198f897535083b64ab4e431a7",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "858349b14a63827a801363675948e40a",
+"assets/LICENSE": "2152acf3d04ccbf049b7b8a82b1d77c2",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b1618cdcdb3af76cbb258736ca56c916",
 "/": "b1618cdcdb3af76cbb258736ca56c916",
-"main.dart.js": "ef8d558cc450f636e5906aac203babb4",
+"main.dart.js": "6a1a05b00c7c9dffce438e175b25edfd",
 "manifest.json": "a55c7bbb21f15d6d435190fc4d59555d"
 };
 
